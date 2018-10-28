884bb4d26c10873bd4532c1c6c655c9b0cf36e5c
Akinca <akinagca@hotmail.com>
2018-10-29 02:26:48 +0300

    Repository ve UnitOfWork generic yapısı oluşturuldu.

---
+++ Cosmos.WebApi/Cosmos.Model/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.Model.Repositories;
+
+namespace Cosmos.Model.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+        int SaveChanges();
+    }
+}
