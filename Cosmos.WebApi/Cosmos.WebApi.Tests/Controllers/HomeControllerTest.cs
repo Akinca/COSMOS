f2d787b5ddcf4d0f6b32e32a90742a947c1c7079
Akinca <akinagca@hotmail.com>
2018-10-28 20:52:12 +0300

    first commit

---
+++ Cosmos.WebApi/Cosmos.WebApi.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cosmos.WebApi;
+using Cosmos.WebApi.Controllers;
+
+namespace Cosmos.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
